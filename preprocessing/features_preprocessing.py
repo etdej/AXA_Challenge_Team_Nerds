efda71efdeaa8a0eb043026dce8fa02a9e457bf4
MC-Bourguiba <med.chedly.bourguiba@gmail.com>
2016-02-14 17:41:39 +0100

    fixed bug

--- preprocessing/features_preprocessing.py
+++ preprocessing/features_preprocessing.py
@@ -12,5 +12,5 @@ preprocessed_data = pd.read_csv("../../data/preprocessed_data.csv", sep=";")
 grouped_by_date_data = preprocessed_data
 
 grouped_by_date_data["DATE"] = preprocessed_data ["DATE"].apply(date_reducer)
-grouped_by_date_data = grouped_by_date_data.groupby(['DATE','ASS_ASSIGNMENT']).sum()
+grouped_by_date_data = grouped_by_date_data.groupby(['DATE','ASS_ASSIGNMENT','DAY_OFF','WEEK_DAY']).sum()
 print(grouped_by_date_data)
