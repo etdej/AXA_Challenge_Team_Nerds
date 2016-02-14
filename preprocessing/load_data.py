9d2663a4fb4a468c6d28667ca74207ebbfa0a665
Etienne Dejoie <etiennedejoie@gmail.com>
2016-02-14 16:01:34 +0100

    change preprocessing. Keep the result in csv file

--- preprocessing/load_data.py
+++ preprocessing/load_data.py
@@ -20,14 +20,16 @@ def find_day(day) :
 class load_data:
 
     def __init__(self):
-        data = pd.read_csv("../../data/train_2011_2012.csv", sep=";", nrows=10000, usecols = CONFIG.useful_columns)
+        data = pd.read_csv("../../data/train_2011_2012.csv", sep=";", usecols = CONFIG.useful_columns)
         week_day = data['DAY_WE_DS'].map(lambda day: find_day(day))
-        data.add(pd.Series(week_day), axis='columns')
-        data.remove(['DAY_DS', 'WEEK_END', 'DAY_WE_DS'])
-        self.data = data
+        data['WEEK_DAY'] = week_day
+        data = data.drop(['DAY_DS', 'WEEK_END', 'DAY_WE_DS'], axis = 1)
+        grouped = data.groupby(['DATE', 'ASS_ASSIGNMENT', 'DAY_OFF', 'WEEK_DAY']).sum()
+
+        print(grouped.sort_values(['CSPL_CALLS'], ascending = [0]))
+        self.data = grouped
 
 if __name__ == "__main__":
     data = load_data()
+    data.data.to_csv('../../data/preprocessed_data.csv', sep=";")
 
-    print(data.data.columns)
-    print(data.data[['']].sort(['CSPL_CALLS'], ascending = [0]))
