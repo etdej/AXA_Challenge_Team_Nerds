afc950751a050f44fc9754abfdfe375e8fe4ded0
MC-Bourguiba <med.chedly.bourguiba@gmail.com>
2016-02-23 14:58:17 +0100

    pipeline tested and works fine

--- model/model.py
+++ model/model.py
@@ -3,6 +3,7 @@ import os
 sys.path.append(os.path.abspath("../preprocessing/"))
 import features_preprocessing as fp
 import submission_preprocessing as sp
+import submission_postprocess as pp
 from sklearn import cross_validation
 from sklearn.cross_validation import cross_val_score
 from sklearn.ensemble import RandomForestRegressor
@@ -12,31 +13,38 @@ from sklearn import linear_model
 
 data_preprocessing = fp.feature_preprocessing()
 data_preprocessing.full_preprocess()
-
+submission = sp.submission_preprocessing()
+submission.full_preprocess()
 
 
 data = data_preprocessing.data
+data_to_predict = submission.data
+Y_to_predict = data_to_predict['CSPL_CALLS']
+X_used_for_prediction = data_to_predict.drop(['CSPL_CALLS'],axis=1)
 Y = data['CSPL_CALLS']
 X= data.drop(['CSPL_CALLS'], axis=1)
 X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, Y, test_size=0.4, random_state=0)
+
 #RandomForest
 
 
-clf = RandomForestRegressor(n_estimators=1000, oob_score=True)
+clf = RandomForestRegressor(n_estimators=100, oob_score=True)
 clf.fit(X_train,Y_train)
 print(clf.score(X_test,Y_test))
 scores = cross_val_score(clf, X ,Y)
 
-predict = clf.predict(X_test)
+predict = clf.predict(X_used_for_prediction)
 
-result = predict-Y_test
+postprocess = pp.submission_postprocess()
+postprocess.premier_submit(predict)
+
 
 
 
 #AdaboostRegression
 
-clf2 = AdaBoostRegressor(n_estimators=100)
-clf2.fit(X_train,Y_train)
-predict_2 = clf2.predict(X_test)
-print(clf2.score(X_test,Y_test))
+#clf2 = AdaBoostRegressor(n_estimators=100)
+#clf2.fit(X_train,Y_train)
+#predict_2 = clf2.predict(X_test)
+#print(clf2.score(X_test,Y_test))
 
--- preprocessing/submission_postprocess.py
+++ preprocessing/submission_postprocess.py
@@ -11,10 +11,10 @@ def date_reducer(date):
     day = str(date[2])
     return year + "-" + month + "-" + day + " " + hour + "-" + minute + "-00.000"
 
-class submission_postprocess(prediction_vector):
+class submission_postprocess():
     
     def __init__(self):
-        self.prediction_vector = prediction_vector
+        print(0)
     
     def transform_vector_to_date(self):
         for key, month in CONFIG.months.items():
@@ -26,10 +26,10 @@ class submission_postprocess(prediction_vector):
         
         self.data["DATE"] = self.data["DATE"].apply(date_reducer)
         
-    def premier_submit(self):
+    def premier_submit(self,pred):
         self.data = pd.read_csv("../../data/submission.txt", sep="\t")
-        self.data['prediction'] = self.prediction_vector
-        self.data.to_csv("result_submission_axa.txt", sep='\t', encoding='utf-8')
+        self.data['prediction'] = pred
+        self.data.to_csv("result_submission_axa.txt", sep='\t', encoding='utf-8', index=False)
     
 if __name__ == "__main__":  
-    
+    print(0)
